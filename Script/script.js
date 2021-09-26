71460b6a88effc8f201e3fd27f57d1ddc8b30e5e
Emmanuel <ogankpaugo@gmail.com>
2021-09-26 23:42:07 +0100

    updating files

--- Script/script.js
+++ Script/script.js
@@ -1,5 +1,7 @@
 const navTogggle = document.querySelector('.nav__toggle');
 const navLinks = document.querySelectorAll('.nav__links');
+window.addEventListener('scroll', show__left);
+window.addEventListener('scroll', show__right);
 
 
 navTogggle.addEventListener('click', () => {
@@ -11,3 +13,35 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav__open')
     })
 })
+
+function show__left(){
+    var show = document.querySelectorAll('.show__left');
+    for (var i = 0; i < show.length; i++){
+        var showHeight = window.innerHeight;
+        var showLeft = show[i].getBoundingClientRect().top;
+        var showPoint = 150;
+
+        if (showLeft < showHeight - showPoint){
+            show[i].classList.add('active');
+        }
+        else{
+            show[i].classList.remove('active');
+        }
+    }
+}
+
+function show__right(){
+    var show = document.querySelectorAll('.show__right');
+    for (var i = 0; i < show.length; i++){
+        var showHeight = window.innerHeight;
+        var showLeft = show[i].getBoundingClientRect().top;
+        var showPoint = 150;
+
+        if (showLeft < showHeight - showPoint){
+            show[i].classList.add('active');
+        }
+        else{
+            show[i].classList.remove('active');
+        }
+    }
+}
